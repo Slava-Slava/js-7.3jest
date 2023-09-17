085b02de8670f319999dcdbfe11a5604e5e918cd
Slava Vasiliev <sb-fantom@yandex.ru>
2023-09-17 23:30:54 +0300

    README.md

--- playwright/tests/App.spec.js
+++ playwright/tests/App.spec.js
@@ -1,22 +1,39 @@
 const { test, expect } = require("@playwright/test");
+const { chromium } = require("playwright");
+const { user, password } = require("../user.js");
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
+test('SuccessfulAuthorization', async ({ page }) => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 5000,
+  });
+  await page.goto('https://netology.ru/');
+  await page.getByRole('link', { name: 'Войти' }).click();
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(user);
+  await page.getByPlaceholder('Пароль').click();
+  await page.getByPlaceholder('Пароль').fill(password);
+  await page.getByTestId('login-submit-btn').click();
+  await expect(page).toHaveTitle("Моё обучение");
+  await page.close;
+  await browser.close;
+});
 
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
-
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
-
-  page.click("text=Бизнес и управление");
-
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
+test('UnsuccessfulAuthorization', async ({ page }) => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 5000,
+  });
+  await page.goto('https://netology.ru/');
+  await page.getByRole('link', { name: 'Войти' }).click();
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(user);
+  await page.getByPlaceholder('Пароль').click();
+  await page.getByPlaceholder('Пароль').fill('password');
+  await page.getByTestId('login-submit-btn').click();
+  await expect(page.getByTestId("login-error-hint")).toHaveText(
+    "Вы ввели неправильно логин или пароль"
   );
+  await page.close;
+  await browser.close;
 });
